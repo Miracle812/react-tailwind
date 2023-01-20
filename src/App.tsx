bdf82eb2a992e92479357b9fcfd5b00d41c3fdb5
kunkka <whoelse857@gmail.com>
2023-01-19 19:47:43 -0500

    second updated

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Body from "./components/Body"
+// this is test
+function App() {
+    return (
+        <div>
+            <header className="text-center">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Body />
+        </div>
+    )
+}
+
+export default App
